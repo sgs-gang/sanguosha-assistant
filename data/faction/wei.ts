e9beb6ac26ea4fbc6c149089360b51b40bcfccb4
Edmond Shen <egsmond@gmail.com>
2025-01-21 17:06:47 +1300

    add explanation cao cao

--- data/faction/wei.ts
+++ data/faction/wei.ts
@@ -11,6 +11,7 @@ export const wei: Character[] = [
         name: "Villainous Hero 奸雄 (jiān xióng)",
         description:
           "You can immediately acquire the card (or cards) that resulted in you suffering damage.",
+          explanation: "Example 1 - When he is the target of ATTACK 杀, Cao Cao can choose not use DODGE 闪, suffer the 1 unit of damage, and acquire the ATTACK card that was used. \n\nExample 2 - When he is recipient of fire damage that has been transmitted via IRON SHACKLES 铁索连环, he receives the card that caused the fire damage even though he was not the initial target. \nExample 3 (complex scenario) - When he uses DODGE to evade an ATTACK but the attacker has equipped Stone Cleaving Axe 贯石斧 and chooses to discard 2 cards to cause damage anyways, Cao Cao suffers the damage but cannot pick up BOTH of the cards that were discarded to cause the damage. This is because the card that causes the damage is the ATTACK and not the discarded cards. Contrast this to the Serpent Halberd 丈八蛇矛, where the 2 cards used in that instance represents an ATTACK card, and thus Cao Cao can pick up both of those cards if he suffers damage."
       },
       {
         name: "Royal Escort 护驾 (hù jià)",
