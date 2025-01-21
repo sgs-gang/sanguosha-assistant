edc9c952858263dd2715af0bc98393c449b9016e
Kneesal <nisalcottingham@gmail.com>
2025-01-21 16:18:47 +1300

    call main

--- scripts/scraper/main.ts
+++ scripts/scraper/main.ts
@@ -1,12 +1,14 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
 async function main(): Promise<void> {
   console.log("scraper starting");
 
-const getReddit = async () => {
-	const response = await fetch('https://reddit.com/');
-	const body = await response.text();
-	console.log(body); // prints a chock full of HTML richness
-	return body;
-};
+  const getReddit = async () => {
+    const response = await fetch("https://reddit.com/");
+    const body = await response.text();
+    console.log(body); // prints a chock full of HTML richness
+    return body;
+  };
 }
+
+main();
