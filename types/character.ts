1dc27a06d7989512d166cebec5740d1dd2bd97d3
Tataihono Nikora <tataihono.nikora@gmail.com>
2025-01-21 16:31:27 +1300

    chore: add characters

--- types/character.ts
+++ types/character.ts
@@ -3,6 +3,11 @@ export interface Character {
   name: string;
   faction: "shu" | "wei" | "wu" | "heroes" | "demi-gods";
   imageUrl: string;
-  abilities: { name: string; description: string }[];
+  abilities: {
+    name: string;
+    description: string;
+    ruler?: true;
+    enforced?: true;
+  }[];
   description?: string;
 }
