73817cf261e0854ce9307ade55560a244cf6afe1
Edmond Shen <egsmond@gmail.com>
2025-01-21 16:06:05 +1300

    add cheerio fetch

--- scripts/scraper/main.ts
+++ scripts/scraper/main.ts
@@ -1,3 +1,12 @@
+import fetch from 'node-fetch';
+
 async function main(): Promise<void> {
   console.log("scraper starting");
+
+const getReddit = async () => {
+	const response = await fetch('https://reddit.com/');
+	const body = await response.text();
+	console.log(body); // prints a chock full of HTML richness
+	return body;
+};
 }
