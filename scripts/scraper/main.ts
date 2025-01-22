2764499e8196c28aabfc75e23010714bac6a24ba
Kneesal <nisalcottingham@gmail.com>
2025-01-22 22:13:20 +1300

    scraper

--- scripts/scraper/main.ts
+++ scripts/scraper/main.ts
@@ -1,34 +1,23 @@
-import fetch from "node-fetch";
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
 import * as cheerio from "cheerio";
 
 async function performScraping(url: string) {
-  const axiosResponse = await axios.request({
-    method: "GET",
-    url,
-    headers: {
-      "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
-    },
-  });
-  return axiosResponse;
+  try {
+    const axiosResponse = await axios.request({
+      method: "GET",
+      url,
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+      },
+    });
+    return axiosResponse;
+  } catch (error) {
+    throw new Error(`Error fetching URL: ${url}, ${error}`);
+  }
 }
 
-// function getCharacterUrls(res: string) {
-//   const characterLinks: string[] = [];
-//   const $ = cheerio.load(res);
-//   const a = $(
-//     `a[imageanchor="1"][href*="http://sanguoshaenglish.blogspot.com/"]`
-//   );
-//   a.each((_i, element) => {
-//     const href = $(element).attr("href");
-//     if (href != null) characterLinks.push(href);
-//   });
-
-//   return characterLinks;
-// }
-
-function getCharacterUrls(res: string) {
+function getCharacterUrls(data: string) {
   const characterLinks: Record<string, string[]> = {
     shu: [],
     wei: [],
@@ -36,15 +25,16 @@ function getCharacterUrls(res: string) {
     heroes: [],
     demiGods: [],
   };
-  let tempLinks: string[] = [];
-  const $ = cheerio.load(res);
+  const $ = cheerio.load(data);
   const shu = $("div.separator")
     .eq(0)
     .children(
       `a[imageanchor="1"][href*="http://sanguoshaenglish.blogspot.com/"]`
     )
     .each((_i, element) => {
-      const href = $(element).attr("href");
+      const href = $(element)
+        .attr("href")
+        ?.replace(/^http:\/\//, "https://");
       if (href != null) characterLinks.shu.push(href);
     });
 
@@ -91,17 +81,91 @@ function getCharacterUrls(res: string) {
   return characterLinks;
 }
 
+function parseCharacterAbilities(text: string) {
+  let ability;
+  const abilityRegex =
+    // /Character ability (\d+):(?: \[Ruler Ability\])? "([^"]+)"\s*([\s\S]+?)(?=Character ability \d+:|$)/g;
+    // /Character ability: "([^"]+)"\s*([\s\S]+?)(?=Character ability:|$)/g;
+    // /Character ability(?: \d+)?:\s?"([^"]+)"\s*([\s\S]+?)(?=\nCharacter ability(?: \d+)?:|$)/g;
+    /Character ability(?: \d+)?:\s*(?:\[[^\]]*\]\s*)?"([^"]+)"\s*([\s\S]+?)(?=\nCharacter ability(?: \d+)?:|$)/g;
+
+  let match;
+
+  if ((match = abilityRegex.exec(text)) !== null) {
+    const [_, id, name, description] = match;
+    ability = { name, description: description?.trim(), ruler: false };
+    if (text.includes("Ruler Ability")) {
+      ability.ruler = true;
+    }
+  } else {
+    throw new Error(`did not match: ${text}`);
+    console.log(text);
+  }
+
+  return ability;
+}
+
+async function addCharacter(url: string, faction: string): Promise<void> {
+  const characterData = await performScraping(url);
+  const $ = cheerio.load(characterData.data);
+  const name = $("h2.post-title").text().replace(/\n+/g, "").trim();
+
+  const imageUrl = $("div.post-body img").first().attr("src");
+  let abilities: { name: string; description: string }[] = [];
+  $('b:contains("Character ability")')
+    .parent()
+    .each((i, el) => {
+      abilities.push(parseCharacterAbilities($(el).text()));
+    });
+
+  // Extract description
+  const description = $("div.post-body")
+    .children("b")
+    .text()
+    .replace(/^Translated description:"/, "");
+  console.log(description);
+
+  // Create the character object
+  const character = {
+    id: name.toLowerCase().replace(/\s+/g, "-"),
+    name,
+    faction,
+    imageUrl,
+    abilities,
+    description,
+  };
+
+  console.log(character);
+}
+
 async function main(): Promise<void> {
   console.log("scraper starting");
 
-  const res = await performScraping(
+  const allCharactersPage = await performScraping(
     "https://sanguoshaenglish.blogspot.com/p/all-characters.html"
   );
-  const characterURLs = getCharacterUrls(res.data);
-  console.log(characterURLs);
-  //   for (const url of characterURLs) {
-  //     const scrape = await performScraping(url);
-  //   }
+  const characterURLs = getCharacterUrls(allCharactersPage.data);
+
+  console.log(characterURLs.shu);
+
+  for (const url of characterURLs.shu) {
+    await addCharacter(url, "shu");
+  }
+  for (const url of characterURLs.wei) {
+    await addCharacter(url, "wei");
+  }
+
+  for (const url of characterURLs.wu) {
+    await addCharacter(url, "wu");
+  }
+
+  for (const url of characterURLs.heroes) {
+    await addCharacter(url, "heroes");
+  }
+
+  for (const url of characterURLs.demiGods) {
+    await addCharacter(url, "demi-gods");
+  }
 }
 
 main();
