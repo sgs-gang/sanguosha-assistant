76b1be2f5eb8c87b83b7ec54003e81e4fc3142ef
Tataihono Nikora <tataihono.nikora@gmail.com>
2025-02-01 18:15:40 -0500

    chore: simplify list

---
+++ scripts/scraper/lib/getList/getList.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod'
+import { parse } from 'async-csv'
+
+export async function getList<T>(
+  url: string,
+  schema: z.ZodType<T>,
+): Promise<T[]> {
+  const res = await fetch(url, {
+    method: 'get',
+    headers: {
+      'content-type': 'text/csv;charset=UTF-8',
+    },
+  })
+
+  const data = await parse(await res.text())
+
+  return schema.array().parse(data)
+}
--- scripts/scraper/main.ts
+++ scripts/scraper/main.ts
@@ -1,9 +1,29 @@
 import colors from 'ansi-colors'
 import { SingleBar } from 'cli-progress'
 import { chunk } from 'lodash'
-import { getList as getCharacters, addItem as addCharacter } from './character'
+import {
+  LIST_URL as CHARACTER_LIST_URL,
+  schema as characterSchema,
+  add as addCharacter,
+} from './character'
+import {
+  LIST_URL as EQUIPMENT_LIST_URL,
+  schema as equipmentSchema,
+  add as addEquipment,
+} from './equipment'
+import {
+  LIST_URL as CARD_LIST_URL,
+  schema as cardSchema,
+  add as addCards,
+} from './card'
+import { z } from 'zod'
+import { getList } from './lib/getList'
 
-async function main(): Promise<void> {
+async function pull<T>(
+  listUrl: string,
+  schema: z.ZodType<T>,
+  addItem: (item: T) => Promise<unknown>,
+): Promise<void> {
   const bar = new SingleBar({
     format: `${colors.cyan(
       '{bar}',
@@ -13,19 +33,25 @@ async function main(): Promise<void> {
     hideCursor: true,
     clearOnComplete: true,
   })
-  const characters = await getCharacters()
+  const items = await getList(listUrl, schema)
 
-  bar.start(characters.length, 0)
-  const chunks = chunk(characters, 10)
+  bar.start(items.length, 0)
+  const chunks = chunk(items, 10)
   for await (const chunk of chunks) {
     await Promise.all(
-      chunk.map(async character => {
-        await addCharacter(character)
+      chunk.map(async item => {
+        await addItem(item)
         bar.increment()
       }),
     )
   }
   bar.stop()
+}
+
+async function main(): Promise<void> {
+  await pull(CHARACTER_LIST_URL, characterSchema, addCharacter)
+  await pull(EQUIPMENT_LIST_URL, equipmentSchema, addEquipment)
+  await pull(CARD_LIST_URL, cardSchema, addCards)
 }
 
 main()
