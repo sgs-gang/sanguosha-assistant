af948395e4285726abfa4cc9ca10d088a22017be
Tataihono Nikora <tataihono.nikora@gmail.com>
2025-02-01 16:30:32 -0500

    change meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'San Guo Sha Assistant',
+  description: 'Helping you understand the game better',
 }
 
 export default function RootLayout({
