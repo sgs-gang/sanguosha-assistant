0208ddcc2ca6dc61b6b9816f6c069eb3e8620560
Tataihono Nikora <tataihono.nikora@gmail.com>
2025-02-01 21:10:44 -0500

    chore: update lint

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+})
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+    ignorePatterns: ['.next/', 'out/'],
+  }),
+]
+export default eslintConfig
