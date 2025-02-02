19070ad89ee541aa5622566ebf2c5e60af6cc22e
Edmond Shen <egsmond@gmail.com>
2025-02-01 21:29:03 -0500

    fix: sort by kingdom

--- data/character.ts
+++ data/character.ts
@@ -8,9 +8,9 @@ export const characters = schema.array().parse(data)
 
 export const factions = [
   { value: 'all', label: 'All', color: '#000000' },
-  { value: 'shu', label: 'Shu', color: '#A7614A' },
-  { value: 'wei', label: 'Wei', color: '#444F7F' },
-  { value: 'wu', label: 'Wu', color: '#789664' },
-  { value: 'heroes', label: 'Hero', color: '#7B767C' },
-  { value: 'demi-gods', label: 'Demi Gods', color: '#D0B260' },
+  { value: 'Shu', label: 'Shu', color: '#A7614A' },
+  { value: 'Wei', label: 'Wei', color: '#444F7F' },
+  { value: 'Wu', label: 'Wu', color: '#789664' },
+  { value: 'Hero', label: 'Hero', color: '#7B767C' },
+  { value: 'God', label: 'Gods', color: '#D0B260' },
 ]
