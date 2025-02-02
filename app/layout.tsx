08b9d7d160bed1046a31a8ee539e64a58f4a28d5
Kneesal <nisalcottingham@gmail.com>
2025-02-03 04:12:05 +1300

    fix: retain favourites accross routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import './globals.css'
 import '@radix-ui/themes/styles.css'
 import { Theme } from '@radix-ui/themes'
 
-import NextLink from 'next/link'
-import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { TabNavigation } from '@/components/tab-navigation'
 
 export default function RootLayout({
--- components/tab-navigation.tsx
+++ components/tab-navigation.tsx
@@ -3,19 +3,23 @@
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { ReactElement } from 'react'
 import NextLink from 'next/link'
+import { useSearchParams } from 'next/navigation'
 
 export function TabNavigation(): ReactElement {
+  const searchParams = useSearchParams()
+  const params = searchParams.toString()
+
   return (
     <Tabs defaultValue="account" className="w-full">
       <TabsList className="w-full">
         <TabsTrigger value="characters">
-          <NextLink href={`/characters`}>Characters</NextLink>
+          <NextLink href={`/characters?${params}`}>Characters</NextLink>
         </TabsTrigger>
         <TabsTrigger value="cards">
-          <NextLink href={`/cards`}>Cards</NextLink>
+          <NextLink href={`/cards?${params}`}>Cards</NextLink>
         </TabsTrigger>
         <TabsTrigger value="equipment">
-          <NextLink href={`/equipment`}>Equipment</NextLink>
+          <NextLink href={`/equipment?${params}`}>Equipment</NextLink>
         </TabsTrigger>
       </TabsList>
     </Tabs>
